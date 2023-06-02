63f8484bb4d1406121fac01eac3292ed52247470
Vincent JAILLOT <vjaillot@cirilgroup.com>
2023-06-02 12:28:29 +0200

    wip clean code

--- src/Core/3DTiles/C3DTBoundingVolume.js
+++ src/Core/3DTiles/C3DTBoundingVolume.js
@@ -5,11 +5,99 @@ import Coordinates from '../Geographic/Coordinates';
 import C3DTilesTypes from './C3DTilesTypes';
 
 const matrix = new THREE.Matrix4();
-const center = new THREE.Vector3();
 const size = new THREE.Vector3();
 const extent = new Extent('EPSG:4326', 0, 0, 0, 0);
+const boxCenter = new THREE.Vector3();
 const sphereCenter = new THREE.Vector3();
 const worldCoordinateCenter = new THREE.Vector3();
+
+const epsilon = 0.00000000000001;
+const regionCenterCarto = new Coordinates('EPSG:4326');
+const regionCenterCartesian = new Coordinates('EPSG:4978');
+const regioncenterVec3 = new THREE.Vector3();
+function computeRegionCenter(east, west, south, north, minHeight, maxHeight) {
+    if (east < west) {
+        east += 2 * Math.PI;
+    }
+
+    let longitude = (west + east) * 0.5;
+    if (longitude < -Math.PI || longitude > Math.PI) {
+        longitude += Math.PI;
+        if (longitude < 0 || longitude > 2 * Math.PI) {
+            const mod = longitude % (2 * Math.PI);
+            if (
+                Math.abs(mod) < epsilon &&
+                Math.abs(longitude) > epsilon
+            ) {
+                longitude = Math.PI * 2;
+            } else {
+                longitude = mod;
+            }
+        }
+    }
+    let latitude = (south + north) * 0.5;
+
+    longitude = THREE.MathUtils.radToDeg(longitude);
+    latitude = THREE.MathUtils.radToDeg(latitude);
+    const height = (maxHeight + minHeight) / 2;
+
+    regionCenterCarto.setFromValues(longitude, latitude, height);
+    regionCenterCarto.as('EPSG:4978', regionCenterCartesian);
+    regionCenterCartesian.toVector3(regioncenterVec3);
+    return regioncenterVec3;
+}
+
+const southWestBottomCarto = new Coordinates('EPSG:4326');
+const southWestBottomCartesian = new Coordinates('EPSG:4978');
+const southWestBottomVec3 = new THREE.Vector3();
+const southEastBottomCarto = new Coordinates('EPSG:4326');
+const southEastBottomCartesian = new Coordinates('EPSG:4978');
+const southEastBottomVec3 = new THREE.Vector3();
+const northWestBottomCarto = new Coordinates('EPSG:4326');
+const northWestBottomCartesian = new Coordinates('EPSG:4978');
+const northWestBottomVec3 = new THREE.Vector3();
+const southWestUpCarto = new Coordinates('EPSG:4326');
+const southWestUpCartesian = new Coordinates('EPSG:4978');
+const southWestUpVec3 = new THREE.Vector3();
+function computeRegionHalfSize(east, west, south, north, minHeight, maxHeight) {
+    // TODO : need to apply rotations for computation to be correct like in the center computation?
+    const westDeg = THREE.MathUtils.radToDeg(west);
+    const eastDeg = THREE.MathUtils.radToDeg(east);
+    const southDeg = THREE.MathUtils.radToDeg(south);
+    const northDeg = THREE.MathUtils.radToDeg(north);
+
+    southWestBottomCarto.setFromValues(westDeg, southDeg, minHeight);
+    southWestBottomCarto.as('EPSG:4978', southWestBottomCartesian);
+    southWestBottomCartesian.toVector3(southWestBottomVec3);
+
+    southEastBottomCarto.setFromValues(eastDeg, southDeg, minHeight);
+    southEastBottomCarto.as('EPSG:4978', southEastBottomCartesian);
+    southEastBottomCartesian.toVector3(southEastBottomVec3);
+
+    northWestBottomCarto.setFromValues(westDeg, northDeg, minHeight);
+    northWestBottomCarto.as('EPSG:4978', northWestBottomCartesian);
+    northWestBottomCartesian.toVector3(northWestBottomVec3);
+
+    southWestUpCarto.setFromValues(westDeg, southDeg, maxHeight);
+    southWestUpCarto.as('EPSG:4978', southWestUpCartesian);
+    southWestUpCartesian.toVector3(southWestUpVec3);
+
+    const sizeX = southWestBottomVec3.distanceTo(southWestUpVec3);
+    const sizeY = southWestBottomVec3.distanceTo(southEastBottomVec3);
+    const sizeZ = southWestBottomVec3.distanceTo(northWestBottomVec3);
+
+    return new THREE.Vector3(sizeX / 2, sizeY / 2, sizeZ / 2);
+}
+
+// center = bounding box / region center in EPSG:4326
+function computeRegionRotation(center) {
+    const euler = new THREE.Euler(0, THREE.MathUtils.degToRad(-center.latitude), THREE.MathUtils.degToRad(center.longitude));
+    const mat4Rot = new THREE.Matrix4();
+    mat4Rot.makeRotationFromEuler(euler);
+    const mat3Rot = new THREE.Matrix3();
+    mat3Rot.setFromMatrix4(mat4Rot);
+    return mat3Rot;
+}
 
 /**
  * @classdesc 3D Tiles
@@ -49,78 +137,18 @@ class C3DTBoundingVolume {
     }
 
     initBoundingRegion(region, inverseTileTransform) {
-        // TODO optim
-        // Try to initialize Box3  from center and size
-        // Compute center
-        let east = region[2];
+        const east = region[2];
         const west = region[0];
         const south = region[1];
         const north = region[3];
         const minHeight = 0;
         const maxHeight = region[5];
 
-        if (east < west) {
-            east += 2 * Math.PI;
-        }
+        const c = computeRegionCenter(east, west, south, north, minHeight, maxHeight);
+        const s = computeRegionHalfSize(east, west, south, north, minHeight, maxHeight);
+        const r = computeRegionRotation(regionCenterCarto); // TODO: pas top de passer regionCenterCarto
 
-        let longitude = (west + east) * 0.5;
-        if (longitude < -Math.PI || longitude > Math.PI) {
-            longitude += Math.PI;
-            if (longitude < 0 || longitude > 2 * Math.PI) {
-                const mod = longitude % (2 * Math.PI);
-                if (
-                    Math.abs(mod) < 0.00000000000001 &&
-                    Math.abs(longitude) > 0.00000000000001
-                ) {
-                    longitude = Math.PI * 2;
-                } else {
-                    longitude = mod;
-                }
-            }
-        }
-        let latitude = (south + north) * 0.5;
-
-        longitude = THREE.MathUtils.radToDeg(longitude);
-        latitude = THREE.MathUtils.radToDeg(latitude);
-        const height = (maxHeight + minHeight) / 2;
-
-        const center = new Coordinates('EPSG:4326', longitude, latitude, height);
-        const centerVec3 = center.as('EPSG:4978').toVector3();
-
-        // Compute size
-        // TODO: x,y,z might be swapped?
-        const westDeg = THREE.MathUtils.radToDeg(west);
-        const eastDeg = THREE.MathUtils.radToDeg(east);
-        const southDeg = THREE.MathUtils.radToDeg(south);
-        const northDeg = THREE.MathUtils.radToDeg(north);
-        // Project on x, y and z axes ? Or the conversion does that ?
-        const coord1 = new Coordinates('EPSG:4326', westDeg, southDeg, minHeight);
-        const coord2 = new Coordinates('EPSG:4326', eastDeg, southDeg, minHeight);
-        const coord1Vec3 = coord1.as('EPSG:4978').toVector3();
-        const coord2Vec3 = coord2.as('EPSG:4978').toVector3();
-        const sizeY = coord1Vec3.distanceTo(coord2Vec3); // TODO: can be negative?
-
-        const coord3 = new Coordinates('EPSG:4326', westDeg, southDeg, minHeight);
-        const coord4 = new Coordinates('EPSG:4326', westDeg, northDeg, minHeight);
-        const coord3Vec3 = coord3.as('EPSG:4978').toVector3();
-        const coord4Vec3 = coord4.as('EPSG:4978').toVector3();
-        const sizeZ = coord3Vec3.distanceTo(coord4Vec3); // TODO: can be negative?
-
-        const coord5 = new Coordinates('EPSG:4326', westDeg, southDeg, minHeight);
-        const coord6 = new Coordinates('EPSG:4326', westDeg, southDeg, maxHeight);
-        const coord5Vec3 = coord5.as('EPSG:4978').toVector3();
-        const coord6Vec3 = coord6.as('EPSG:4978').toVector3();
-        const sizeX = coord5Vec3.distanceTo(coord6Vec3); // TODO: can be negative?
-        const halfSize = new THREE.Vector3(sizeX / 2, sizeY / 2, sizeZ / 2);
-
-        // Compute rotation
-        const euler = new THREE.Euler(0, THREE.MathUtils.degToRad(-center.latitude), THREE.MathUtils.degToRad(center.longitude));
-        const mat4Rot = new THREE.Matrix4();
-        mat4Rot.makeRotationFromEuler(euler);
-        const mat3Rot = new THREE.Matrix3();
-        mat3Rot.setFromMatrix4(mat4Rot);
-
-        this.region = new OBB(centerVec3, halfSize, mat3Rot);
+        this.region = new OBB(c, s, r);
         this.region.applyMatrix4(inverseTileTransform);
     }
 
@@ -129,10 +157,10 @@ class C3DTBoundingVolume {
         // box[3], box[4], box[5] = x axis direction and half-length
         // box[6], box[7], box[8] = y axis direction and half-length
         // box[9], box[10], box[11] = z axis direction and half-length
-        center.set(box[0], box[1], box[2]);
+        boxCenter.set(box[0], box[1], box[2]);
         size.set(box[3], box[7], box[11]).multiplyScalar(2);
         this.box = new THREE.Box3();
-        this.box.setFromCenterAndSize(center, size);
+        this.box.setFromCenterAndSize(boxCenter, size);
     }
 
     initBoundingSphere(sphere) {
