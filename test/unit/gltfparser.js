c4d616be45cdf3b812a6cb682b2f1fc7a4625b54
Vincent JAILLOT <vjaillot@cirilgroup.com>
2024-01-31 17:09:45 +0100

    wip add tests, need to wait for francois work on unit tests, promises seems to never be resolved

--- test/unit/gltfparser.js
+++ test/unit/gltfparser.js
@@ -1,15 +1,26 @@
+import fs from 'fs';
 import assert from 'assert';
 import GLTFParser from 'Parser/GLTFParser';
-import gltf from '../data/gltf/Box.gltf';
+import gltf20 from '../data/gltf/Box.gltf';
 
 if (typeof atob === 'undefined') {
     global.atob = b64Encoded => Buffer.from(b64Encoded, 'base64').toString('binary');
 }
 
 describe('GLTFParser', function () {
-    it('should load gltf', function () {
-        GLTFParser.parse(gltf, '../data/gltf/', (result) => {
+    // TODO catch + promise / then chercher autres tests comment c'est fait
+    it('should load glb 1.0', function () {
+        const glb10 = fs.readFileSync('test/data/gltf/Box-1.0.glb');
+        GLTFParser.parse(glb10, '../data/gltf/').then((result) => {
+            console.log('succeed');
             assert.ok(result.scene);
         });
     });
+
+    it('should load gltf 2.0', function () {
+        GLTFParser.parse(gltf20, '../data/gltf/').then((result) => {
+            assert.ok(result.scene);
+        });
+    });
+
 });
